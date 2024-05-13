e7f902ae0fc8c11f394a711625ffcd2559cd808f
Denys Chornokon <denyschornokon3@gmail.com>
2024-05-13 15:46:44 +0300

    added movements and charms

--- main.py
+++ main.py
@@ -16,3 +16,71 @@ class Monster(ABC):
         pass
 
 
+class RunningMonster(Monster):
+    def move(self):
+        print(f"{self.name} біжить.")
+
+    def use_charm(self):
+        self.charm.use_quick_regen(self)
+
+
+class FlyingMonster(Monster):
+    def move(self):
+        print(f"{self.name} літає.")
+
+    def use_charm(self):
+        self.charm.use_strong_strike(self)
+
+
+class FlyingRunningMonster(RunningMonster, FlyingMonster):
+    def move(self):
+        print(f"{self.name} може бігати та літати.")
+
+
+class Charm(ABC):
+    @abstractmethod
+    def use_quick_regen(self, monster):
+        pass
+
+    @abstractmethod
+    def use_strong_strike(self, monster):
+        pass
+
+    @abstractmethod
+    def become_immortal(self, monster):
+        pass
+
+
+class QuickRegenCharm(Charm):
+    def use_quick_regen(self, monster):
+        print(f"{monster.name} використовує швидке відновлення сили.")
+
+    def become_immortal(self, monster):
+        pass
+
+    def use_strong_strike(self, monster):
+        pass
+
+
+class StrongStrikeCharm(Charm):
+    def use_quick_regen(self, monster):
+        pass
+
+    def use_strong_strike(self, monster):
+        print(f"{monster.name} використовує посилений удар.")
+
+    def become_immortal(self, monster):
+        pass
+
+
+class ImmortalityCharm(Charm):
+    def use_quick_regen(self, monster):
+        pass
+
+    def use_strong_strike(self, monster):
+        pass
+
+    def become_immortal(self, monster):
+        print(f"{monster.name} стає безсмертним.")
+
+
