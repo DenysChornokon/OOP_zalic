1ad8847e9d0a3c256052e756fc6ec02e159daecb
Denys Chornokon <denyschornokon3@gmail.com>
2024-05-13 15:29:12 +0300

    monster class added

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('test')
+from abc import ABC, abstractmethod
+
+
+class Monster(ABC):
+    def __init__(self, name, movement_type, charm):
+        self.name = name
+        self.movement_type = movement_type
+        self.charm = charm
+
+    @abstractmethod
+    def move(self):
+        pass
+
+    @abstractmethod
+    def use_charm(self):
+        pass
+
+
